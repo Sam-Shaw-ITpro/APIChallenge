c3a33812996923d018ba5aec9d85b87d309ef2eb
Sam Shaw <sam.shaw.itpro@gmail.com>
2018-10-10 10:15:32 -0400

    first commit

---
+++ song.js
@@ -0,0 +1,53 @@
+const baseURL = 'http://www.songsterr.com/a/ra/songs.json?pattern=';
+let url;
+let lTarget;
+const searchForm = document.querySelector('form');
+const submitBtn = document.querySelector('.submit');
+const section = document.querySelector('section');
+
+searchForm.addEventListener('submit', fetchResults);
+
+function fetchResults(e) {
+    e.preventDefault()
+    console.log(e);
+    url = baseURL + search.value;
+    fetch(url)
+    .then(function(result) {
+    console.log(result)
+    return result.json();
+    }).then(function(json) {
+    displayResults(json);
+    });
+}
+  
+function displayResults(json) {
+
+    while (section.firstChild) {
+    section.removeChild(section.firstChild); }
+    let theJsonResponse = json;
+
+    if(theJsonResponse.length === 0) {
+      console.log("No results");
+      } else {
+
+          for(let i = 0; i < theJsonResponse.length && i <= 20; i++) {
+          let article = document.createElement('article');
+          let heading = document.createElement('h4');
+          let link = document.createElement('a');
+          let linkToSong= "http://www.songsterr.com/a/wa/song?id=";
+          link.setAttribute("target", "_blank");
+          
+          let current = theJsonResponse[i];
+          console.log("Current:", current);
+          idNum = current.id;
+          link.href = linkToSong+idNum;
+          artistName = current.artist.name;
+          songName = current.title;
+          link.textContent = '  Play ==>  ' + artistName + "  |  " + songName;
+          article.appendChild(heading);
+          heading.appendChild(link);
+          section.appendChild(article);
+          }
+      }
+};
+
